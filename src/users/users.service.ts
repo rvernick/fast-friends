0357eae6fbfb170740cc24d0b5c438f8c9e3f297
Russell Vernick <rvernick@yahoo.com>
2023-04-11 18:05:23 -0700

    Added Auth service.  Access protection global

---
+++ src/users/users.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+
+export type User = any;
+
+@Injectable()
+export class UsersService {
+  private readonly users = [
+    {
+      userId: 1,
+      username: 'rvernick@yahoo.com',
+      password: 'wuzzle',
+    },
+    {
+      userId: 2,
+      username: 'testUser',
+      password: '1234',
+    },
+  ];
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find((user) => user.username === username);
+  }
+}
