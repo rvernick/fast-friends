dd02fe81d4a3a0536f8cda6133725f3f914d9cc5
Russell Vernick <rvernick@yahoo.com>
2023-04-13 14:34:23 -0700

    Added encryption and a few columns to user

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { EncryptionTransformer } from 'typeorm-encrypted';
 
 @Entity()
 export class User {
@@ -8,7 +15,16 @@ export class User {
   @Column()
   username: string;
 
-  @Column()
+  @Column({
+    type: 'varchar',
+    nullable: false,
+    transformer: new EncryptionTransformer({
+      key: 'C&F)J@NcRfUjXn2r5u8x/A%D*G-KaPdSgVkYp3s6v9y$B&E(H+MbQeThWmZq4t7w',
+      algorithm: 'aes-256-cbc',
+      ivLength: 16,
+      iv: '7w!z%C&F)J@NcRfU',
+    }),
+  })
   password: string;
 
   @Column()
@@ -16,4 +32,13 @@ export class User {
 
   @Column()
   lastName: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @CreateDateColumn()
+  createdOn: Date;
+
+  @UpdateDateColumn()
+  updatedOn: Date;
 }
