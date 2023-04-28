144999544866b44de2ccbe2adb4a8182ee680f08
Russell Vernick <rvernick@yahoo.com>
2023-04-28 08:14:55 -0700

    added health check start

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './auth/auth.guard';
 
-@Controller()
+@Controller('health')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @HttpCode(HttpStatus.OK)
+  @Public()
+  @Get('check')
+  signIn() {
+    return 'Up';
   }
 }
--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import { React, useContext } from 'react';
 //import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { NativeBaseProvider } from 'native-base';
 import { StyleSheet, Text, View } from 'react-native';
