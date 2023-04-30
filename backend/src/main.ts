7b5b02bdcb69a99440ea8592168c655b11e20dd6
Russ Vernick <rvernick@yahoo.com>
2023-04-30 15:44:30 -0700

    Render (#11)
    * add logging
    * added logging with winston logger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,43 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { AppLogger } from './config/app.logger';
+import { LoggerService, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true, });
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    logger: createLogger(),
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
+}
+
+function createLogger(): LoggerService {
+  const winston = require('winston');
+
+  const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+      new winston.transports.File({
+        format: winston.format.simple(),
+        filename: 'logs/error.log',
+        level: 'error',
+        maxsize: 1000,
+        maxFiles: 2,
+        tailable: true,
+      }),
+      new winston.transports.File({
+        format: winston.format.simple(),
+        filename: 'logs/combined.log',
+        level: 'info',
+        maxsize: 1000,
+        maxFiles: 3,
+        tailable: true,
+      }),
+      new winston.transports.Console({ level: 'info', format: winston.format.simple() }),
+    ],
+  });
+  return logger;
 }
 bootstrap();
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Logger, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 
 @Injectable()
 export class UsersService {
+  private readonly logger = new Logger(UsersService.name);
+  
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
@@ -15,16 +17,20 @@ export class UsersService {
   }
 
   findOne(id: number): Promise<User | null> {
-    return this.usersRepository.findOneBy({ id });
+    const result = this.usersRepository.findOneBy({ id });
+    this.logger.log('info', 'Searching for: ' + id + ' found: ' + result);
+    return result;
   }
 
   findUsername(username: string): Promise<User | null> {
     if (username == null) return null;
-    return this.usersRepository.findOne({
+    const result = this.usersRepository.findOne({
       where: {
         username: username,
       },
     });
+    this.logger.log('info', 'Searching for: ' + username + ' found: ' + result);
+    return result;
   }
 
   createUser(username: string, password: string) {
