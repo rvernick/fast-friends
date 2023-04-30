b9e75530b21b5a78605cca51ef04b2f541205f4f
Russell Vernick <rvernick@yahoo.com>
2023-04-30 13:34:20 -0700

    add logging

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { LoggerService, ValidationPipe } from '@nestjs/common';
 import { AppLogger } from './config/app.logger';
+import { create } from 'domain';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true, });
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    logger: createLogger(),
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
+}
+
+function createLogger(): LoggerService {
+  const winston = require('winston');
+
+  const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+      //
+      // - Write all logs with importance level of `error` or less to `error.log`
+      // - Write all logs with importance level of `info` or less to `combined.log`
+      //
+      new winston.transports.File({ filename: 'error.log', level: 'error' }),
+      new winston.transports.File({ filename: 'combined.log' }),
+      new winston.transports.Console({format: winston.format.simple()}),
+    ],
+  });
+  return logger;
 }
 bootstrap();
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Logger, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 
 @Injectable()
 export class UsersService {
+  private readonly logger = new Logger(UsersService.name);
+  
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
@@ -15,7 +17,9 @@ export class UsersService {
   }
 
   findOne(id: number): Promise<User | null> {
-    return this.usersRepository.findOneBy({ id });
+    const result = this.usersRepository.findOneBy({ id });
+    this.logger.log('Searching for: ' + id + ' found: ' + result);
+    return result;
   }
 
   findUsername(username: string): Promise<User | null> {
