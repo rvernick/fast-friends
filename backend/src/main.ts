b7213756d094a6447a0b3acd3f894906034bad22
Russell Vernick <rvernick@yahoo.com>
2023-04-30 16:33:40 -0700

    updating some

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
     logger: createLogger(),
   });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(80);
 }
 
 function createLogger(): LoggerService {
