8d66bdc131ea3d35a8d9f6aa606f2113b21d9bb7
Russell Vernick <rvernick@yahoo.com>
2023-04-30 22:22:06 -0700

    use configed port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { LoggerService, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const port =  3000;
+  const port = process.env.PORT || 3000;
   console.log('Opening on port ' + port);
 
   const app = await NestFactory.create(AppModule, {
