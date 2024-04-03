de38eddcd23f506328a9c57b7785fc4edc91bbf7
Russell Vernick <rvernick@sightmachine.com>
2024-04-03 00:29:06 -0700

    testing to see why it's not getting db info

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -20,9 +20,9 @@ import { User } from './users/user.entity';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: +configService.get('DB_PORT'),
-        username: configService.get('DB_USER'),
+        host: configService.get('DB_HOST', 'localhost'),
+        port: +configService.get('DB_PORT', 5432),
+        username: configService.get('DB_USER', 'postgres'),
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_DATABASE_NAME'),
         entities: [User],
