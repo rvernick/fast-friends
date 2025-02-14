98e5be0173f166c95f916e4a2a9147ba13c23545
Russell Vernick <rvernick@yahoo.com>
2025-02-13 16:27:29 -0800

    PasswordReset WaitForVerification

--- frontend/components/account/PasswordResetComponent.tsx
+++ frontend/components/account/PasswordResetComponent.tsx
@@ -7,7 +7,6 @@ import { BaseLayout } from "../layouts/base-layout";
 import { VStack } from "../ui/vstack";
 import { Heading } from "../ui/heading";
 import { Text } from "../ui/text";
-import { Textarea, TextareaInput } from "../ui/textarea";
 import { Input, InputField } from "../ui/input";
 import { Button, ButtonText } from "../ui/button";
 
