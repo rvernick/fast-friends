3c87c1daf0c4a2a0509c6dcc81d3f322f230b7df
Russell Vernick <rvernick@yahoo.com>
2025-04-24 20:25:51 -0700

    fixed batch process bug and Strava integration as a result

--- backend/src/batch/batch-process.service.ts
+++ backend/src/batch/batch-process.service.ts
@@ -21,6 +21,7 @@ export class BatchProcessService {
     if (result === null) {
       result = await this.batchProcessRepository.create();
       result.name = name;
+      return await this.batchProcessRepository.save(result);
     }
     return result;
   };
@@ -32,18 +33,20 @@ export class BatchProcessService {
 
   async attemptToLock(batchProcess: BatchProcess): Promise<BatchProcess> {
     try {
+      await this.unlockIfOverdue(batchProcess);
       if (batchProcess.lockedKey == null) {
         const lockedKey = Math.random().toString(36).substr(2, 10);
         const lockedOn = new Date();
         this.logger.log('Locking batch process');
         const lockQuery = this.batchProcessRepository.createQueryBuilder();
         const lockedBatchProcess = await lockQuery
-          .update(BatchProcess).set({
+          .update(BatchProcess)
+          .set({
             lockedKey: lockedKey,
             lockedOn: lockedOn,
           })
-          .where({ id: batchProcess.id })
-          .andWhere({ lockedKey: null })
+          .where("id = :id", { id: batchProcess.id })
+          .andWhere("lockedKey IS NULL")
           .execute();
         if (lockedBatchProcess.affected == 1) {
           this.logger.log('Locked batch process successfully');
@@ -63,6 +66,12 @@ export class BatchProcessService {
     } catch (error) {
       this.logger.log('Error locking batch process name: ', error);
       return null;
+    }
+  }
+
+  private async unlockIfOverdue(batchProcess: BatchProcess) {
+    if (batchProcess.lockedKey != null && this.hasBeenLockedForTooLong(batchProcess)) {
+      await this.unlock(batchProcess);
     }
   }
 
@@ -89,20 +98,32 @@ export class BatchProcessService {
     }
   }
 
-  async unlock(batchProcess: BatchProcess) {
+  async unlockAndDelete(batchProcess: BatchProcess) {
+    const unlockedBatchProcess = await this.unlock(batchProcess);
+    if (unlockedBatchProcess!= null) {
+      await this.batchProcessRepository.delete(unlockedBatchProcess.id);
+      this.logger.log('Deleted unlocked batch process ' + JSON.stringify(unlockedBatchProcess));
+    } else {
+      this.logger.log('Unable to unlock batch process'+ JSON.stringify(batchProcess));
+    }
+  }
+
+  async unlock(batchProcess: BatchProcess): Promise<BatchProcess> | null {
     if (batchProcess == null) return;
     try {
       const id = batchProcess.id;
       const currentBatchProcess = await this.batchProcessRepository.findOneBy({ id });
       currentBatchProcess.lockedKey = null;
       currentBatchProcess.lockedOn = null;
-      await this.batchProcessRepository.save(currentBatchProcess);
+      const result = await this.batchProcessRepository.save(currentBatchProcess);
       this.logger.log('Unlocked batch process ' + JSON.stringify(currentBatchProcess));
+      return result;
     } catch (error) {
       this.logger.log('Error finishing batch process name: ', batchProcess.name);
       this.logger.log('Error finishing batch process key: ', batchProcess.lockedKey);
       this.logger.log('Error finishing batch process date: ', batchProcess.lockedOn);
       this.logger.error('Error finishing batch process: ', error);
+      return null;
     }
   }
 }
